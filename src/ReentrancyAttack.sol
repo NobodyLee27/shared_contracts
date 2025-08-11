412dc681c47f15acf0a0de80ea0ad751fc8eb259
leelongxi <lee@birdiecorp.com>
2025-08-11 08:55:20 +0800

    init:sql

--- src/ReentrancyAttack.sol
+++ src/ReentrancyAttack.sol
@@ -18,9 +18,11 @@ contract ReentrancyAttack {
     function withdraw() external {
         uint bal = balances[msg.sender];
         require(bal > 0, "not enough balance");
+        // balances[msg.sender] = 0; // 先更新状态
         // 转入 eth 有重入的风险 因为转账过程中会调用回退函数 回退函数中可以继续调用 withdraw 由于其顺序调用特性 永远不会继续向下执行
         (bool sent, ) = msg.sender.call{value: bal}("");
         require(sent, "failed to send Ether");
+        // 更新余额在后面
         balances[msg.sender] = 0;
     }
 
@@ -50,6 +52,7 @@ contract Attacker {
         banker = ReentrancyAttack(_addr);
     }
 
+    // 低层方法 接收 eth 时调用 如果没有 receive 方法 会调用 fallback 方法
     receive() external payable {
         if (banker.getBalance() >= 1 ether)  {
             banker.withdraw();
